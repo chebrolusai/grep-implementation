a816d87e0625b3b9211c483f837f8934e06e3071
Sai Siddhardha Chebrolu <chebrolu.s@northeastern.edu>
2024-03-06 17:38:36 -0500

    match function and option l handling

--- util.py
+++ util.py
@@ -1,7 +1,8 @@
 import os
+import re
 
-allowed_options = {'i','n','c','l','w'}
-error_text = "\033[1;31mERROR: \033[0m"
+allowed_options = {'i','n','c','l','w','H'}
+ERROR_TEXT = "\033[1;31mERROR: \033[0m"
 
 def validate_input_arguments( search_string,command_options,file_paths ):
     
@@ -9,26 +10,69 @@ def validate_input_arguments( search_string,command_options,file_paths ):
 
         if option not in allowed_options:
             
-            print(error_text + "Invalid option " + option + "\n")
+            print(ERROR_TEXT + "Invalid option " + option + "\n")
             exit(1)
 
     if not search_string:
 
-        print(error_text + "You must specify the search string.\n")
+        print(ERROR_TEXT + "You must specify the search string.\n")
         exit(1)
 
     if not file_paths:
 
-        print(error_text + "You must specify atleast one file.\n")
+        print(ERROR_TEXT + "You must specify atleast one file.\n")
         exit(1)
 
     for filespec in file_paths:
         
         if not os.path.exists(filespec):
-            print(error_text + filespec + " - File path does not exist.\n")
+            print(ERROR_TEXT + filespec + " - File path does not exist.\n")
             exit(2)
 
         elif os.path.isdir(filespec):
-            print(error_text + filespec + " - is a directory.")
+            print(ERROR_TEXT + filespec + " - is a directory.")
             exit(2)
+
+
+def get_pattern_matching_type(command_options,search_string):
+
+    # Pattern Matches
+    CASE_SENSITIVE        = re.compile(search_string)
+    CASE_INSENSITIVE      = re.compile(search_string,re.IGNORECASE)
+    CASE_SENSITIVE_WORD   = re.compile(f'\\b{search_string}\\b')
+    CASE_INSENSITIVE_WORD = re.compile(f'\\b{search_string}\\b',re.IGNORECASE)
+
+    MATCH_TYPE = CASE_SENSITIVE
+
+    if 'w' in command_options:
+
+        MATCH_TYPE = CASE_SENSITIVE_WORD
+
+        if 'i' in command_options:
+
+            MATCH_TYPE = CASE_INSENSITIVE_WORD
     
+    elif 'i' in command_options:
+
+        MATCH_TYPE = CASE_INSENSITIVE
+    
+    return MATCH_TYPE
+
+
+def handle_Hcl_options(command_options,file_name):
+
+    output_string = ''
+
+    if 'c' in command_options:
+
+        if 'H' in command_options:
+
+            output_string = f'{file_name}:1'
+        
+        else:
+
+            output_string = '1'
+        
+        print(output_string)
+    
+    print(file_name)
