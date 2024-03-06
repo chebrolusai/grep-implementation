a787cf75f3ad97ef49565e005860d44bab5e9376
Sai Siddhardha Chebrolu <chebrolu.s@northeastern.edu>
2024-03-06 17:38:36 -0500

    match function and option l handling

--- grep.py
+++ grep.py
@@ -8,13 +8,13 @@ search_string   = ''
 file_paths      = []
 
 
-error_text = "\033[1;31mERROR: \033[0m"
-
+ERROR_TEXT = "\033[1;31mERROR: \033[0m"
+READ_ONLY  = 'r'
 
 # Error handling
 if sys.argv.__len__() < 3:
 
-    print(error_text + "You must specify the search string and the filename.\n")
+    print(ERROR_TEXT + "You must specify the search string and the filename.\n")
     exit(1)
 
 
@@ -37,4 +37,33 @@ for argument in sys.argv[1:]:
         file_paths.append(argument)
 
 
+
 util.validate_input_arguments(search_string,command_options,file_paths)
+
+
+MATCH_TYPE = util.get_pattern_matching_type(command_options,search_string)
+
+
+grep_result_object = {}
+
+for eachfile in file_paths:
+
+    line_count     = 0
+    match_count    = 0
+
+    with open(eachfile,READ_ONLY) as file:
+        
+        for line in file:
+
+            line_count += 1
+            matches     = MATCH_TYPE.findall(line)
+
+            if len(matches):
+
+                match_count += len(matches)
+
+                if 'l' in command_options:
+                    util.handle_Hcl_options(command_options,eachfile)
+                    break
+
+
